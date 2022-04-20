a21a78080da122ee318e8e103c2ec4027e7702ba
izalmfrds <rizalmaidanytb@gmail.com>
2022-04-20 20:10:32 +0700

    person

---
+++ src/lib/Person.java
@@ -0,0 +1,10 @@
+package lib;
+
+public class Person {
+    private String spouseName;
+	private String spouseIdNumber;
+
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+		this.spouseName = spouseName;
+		this.spouseIdNumber = idNumber;
+}
